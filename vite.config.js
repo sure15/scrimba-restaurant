6d3428938ccb82db7f5a4acfd145b9500ece90c2
sure15 <gxishk15@gmail.com>
2026-02-20 02:27:47 +0800

    修改vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import {defineConfig} from "vite"
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [
-		
-	]
+	base: '/scrimba-restaurant/',   // ← 关键
+	build: {
+		outDir: 'docs'
+	}
 })
